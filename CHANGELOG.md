67997fdcd150f06b5373d5b1ae7f32020793ce34
YoriKv <yori.kvitchko@gmail.com>
2026-09-11 01:30:29 -0700

    Release v0.1.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## v0.1.2 - unreleased
+## v0.1.2 - 2026-09-11
 
 - Export actions
 - Shortcuts and shortcuts guide
--- app/dbkai/__init__.py
+++ app/dbkai/__init__.py
@@ -17,7 +17,7 @@ is testable and reusable headless.
   frozen release build.
 """
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 # The name shown to a person: window titles, dialogs, the About box, and
 # QApplication.applicationDisplayName.
